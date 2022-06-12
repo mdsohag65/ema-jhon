5b31e5ec3685817812844d00ef615dcec6aedaf6
mdsohag65 <sohag92ahmed@gmail.com>
2022-06-12 15:57:45 +0600

    navbar done

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Shop = () => {
+    return (
+        <section>
+            <div className="products-container">
+
+            </div>
+            <div className="car-container">
+
+            </div>
+        </section>
+    );
+};
+
+export default Shop;
